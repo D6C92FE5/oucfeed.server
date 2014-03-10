fd19012f39ba85da0ae3d515a266dc51e72c08fb
D6C92FE5 <D6C92FE5@hotmail.com>
2014-03-10 15:34:31 +0800

    适配到 BAE 又一步

---
+++ bae.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from __future__ import division, absolute_import, print_function, unicode_literals
+
+import os
+import sys
+
+
+os.chdir(os.path.dirname(__file__))
+sys.path.insert(0, '.')
+
+
+from oucfeed.server import app as application
